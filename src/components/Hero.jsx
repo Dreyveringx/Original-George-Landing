f5e68667325a069b770508f851756f8654bd71ea
Jair De Jesus Uribe Valencia <xdreyveringx@hotmail.com>
2024-01-06 18:02:32 -0500

    Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,20 +7,20 @@ const Hero = () => {
   return (
     <>
       <Bg />
-      <div className="text-5xl text-white bg-black  bg-opacity-80  min-h-screen flex justify-center items-center">
-        <div className="text-center p-24">
-          <h1 className="text-6xl font-serif font-semibold">
+      <div className="text-5xl text-white bg-black  bg-opacity-80  min-h-screen flex  lg:flex-row flex-col md:items-center justify-center items-center text-center">
+        <div className="text-center p-24 ">
+          <h1 className="md:text-3xl sm:text-3xl ms:text-4xl lg:text-6xl font-serif font-semibold">
             <strong>Original George</strong>
           </h1>
-          <p className="mt-5 text-2xl font-serif font-normal">
+          <p className="mt-5 sm:text-sm md:text-xl lg:text-2xl font-serif font-normal">
             ¡Descubre la revolución en el mundo del trading!
           </p>
-          <p className="mt-5 mb-10 text-xl text-secondary font-serif font-normal">
+          <p className="mt-5 mb-10 sm:text-base md:text-lg lg:text-xl text-secondary font-serif font-normal">
             Únete a la Primera Academia de Trading Gratuita.
           </p>
           <a
             href="#contact"
-            className="button font-serif font-semibold text-xl "
+            className="button font-serif font-semibold sm:text-sm md:text-lg lg:text-xl "
           >
             ¡Únete ahora!
           </a>
@@ -28,7 +28,7 @@ const Hero = () => {
         <img
           src={Perfil}
           alt="Foto de perfil"
-          className="rounded-full"
+          className="rounded-full w-auto h-auto"
           style={{ objectFit: "cover" }}
         />
       </div>
