8fae8c9d24a5fb35a64b799444d3ba0e03fa751f
Jair De Jesus Uribe Valencia <xdreyveringx@hotmail.com>
2024-01-11 21:26:14 -0500

    correción ruta de componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Hero from "./components/Hero.jsx";
 import "./App.css";
 import Information from "./components/Information.jsx";
-import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
